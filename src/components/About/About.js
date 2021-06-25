b31eed384559f6f326e309dfc7d3db4324f9da8b
DESKTOP-U6O47B3\User <ayana.sultanovaa@gmail.com>
2021-06-25 18:36:42 +0300

    Запрос данных с сервера

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,34 +2,77 @@
 import React from 'react';
 import { CardContent } from '@material-ui/core';
 import { LinearProgress } from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
 import { Octokit } from '@octokit/rest';
+import styles from "../About/About.module.css"
 
 const octokit = new Octokit();
 
 class About extends React.Component {
     state = {
         isLoading: true,
-        repoList: []
+        repoList: [],
+        infoAboutUser: [],
+        isError: false
     }
 
     componentDidMount() {
         octokit.repos.listForUser({
-            userName: 'Ayana-js'
-        }).then(({ data }) => {
+            username: 'Ayana-js'
+        })
+        .then(({ data }) => {
             this.setState({
                 repoList: data,
-                isLoading: false
+                isLoading: false,
+                avatarUrl: data[0].owner.avatar_url,
+                login: data[0].owner.login
             });
-        });
+        })
+        .catch(err => {
+            this.setState({ 
+              isLoading: false,
+              isError: true,
+            });
+          });
+
+        octokit.users.getByUsername({
+            username: 'Ayana-js'
+          })
+          .then(({data}) => {
+            this.setState({ 
+              infoAboutUser: data,
+              isLoading: false,
+              avatarUrl: data[0].owner.avatar_url,
+              login: data[0].owner.login
+            });
+          })
+          .catch(err => {
+            this.setState({ 
+              isLoading: false,
+              isError: true,
+            });
+          });
     }
     render () {
-        const { isLoading, repoList } = this.state
+        const { isLoading, repoList, isError, infoAboutUser } = this.state
         return (
             <CardContent>
-                <h1>{ isLoading ? < LinearProgress /> : 'Список репозиториев:' }</h1>
+                <h1 className={styles.title}>{ isLoading ? < LinearProgress /> : 'Список репозиториев:' }</h1>
                 {!isLoading && <ol>
-                        {repoList.map(repo => (<li key={repo.id}>
+                    <Avatar
+                classname={styles.avatar}
+                alt={this.state.repoList.username}
+                src={this.state.avatarUrl}
+              />
+              <div>
+                <h3>{this.state.login}</h3>
+            </div>
+                        {repoList.map(repo => 
+                            (<li className={styles.repos} key={repo.id}>
                             {repo.html_url}
+                            <a className={styles.link} href={repo.html_url}>
+                            {repo.name}
+                    </a>  
                         </li>))}
                     </ol>}
             </CardContent>
